7aa66b666a70362920041cc698ca650bd1471021
jbreidenbach <bbach07@gmail.com>
2013-05-21 15:05:39 -0500

    something

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521150358) do
+ActiveRecord::Schema.define(:version => 20130521195813) do
 
   create_table "campaigns", :force => true do |t|
     t.string   "campaign_name"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20130521150358) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "email"
   end
 
 end
