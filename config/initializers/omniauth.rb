9019f37c07a426fd7bd07f332571350716269b89
jbreidenbach <bbach07@gmail.com>
2013-06-13 15:46:16 -0500

    Pre SF

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,10 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :facebook, '588417664516856', 'f19d4d753e9265ca10af575639544ef5'
+    provider :facebook, '588417664516856', 'f19d4d753e9265ca10af575639544ef5',
+    :scope => 'email,user_education_history', :display => 'popup'
   else
-    provider :facebook, '227668764045536', 'f6c3f56f2f07474892d0848721817cee'
+    provider :facebook, '227668764045536', 'f6c3f56f2f07474892d0848721817cee',
+    :scope => 'email,user_education_history', :display => 'popup'
   end
 end
